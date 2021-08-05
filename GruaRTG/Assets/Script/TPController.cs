738dcf23636ab2714eecd1cf41b5060114e3535a
Santiago Zabib <santiago_zabib@hotmail.com>
2021-08-05 12:15:50 -0300

    TPSController1
    Script movimientos de personaje

--- GruaRTG/Assets/Script/TPController.cs
+++ GruaRTG/Assets/Script/TPController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class TPController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CharacterController personaje;
+
+    public float walkSpeed = 6.0f;
+    public float runSpeed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 move = Vector3.zero;
+
     void Start()
     {
-        
+        personaje = GetComponent<CharacterController>();    
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (personaje.isGrounded)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                move = transform.TransformDirection(move) * runSpeed;
+            }
+            else
+            {
+                move = transform.TransformDirection(move) * walkSpeed;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                move.y = jumpSpeed;
+            }
+        }
+        move.y -= gravity * Time.deltaTime;
+        personaje.Move(move * Time.deltaTime);
     }
 }

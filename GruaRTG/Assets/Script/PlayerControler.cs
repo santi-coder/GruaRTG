cbf8ba2a58423a45b9c5dff7080d0e43e8877d86
Santiago Zabib <santiago_zabib@hotmail.com>
2021-08-06 17:10:30 -0300

    Animaciones listas

--- GruaRTG/Assets/Script/PlayerControler.cs
+++ GruaRTG/Assets/Script/PlayerControler.cs
@@ -6,18 +6,26 @@ public class PlayerControler : MonoBehaviour
 {
     CharacterController player;
 
-    public float walkSpeed = 6.0f;
-    public float runSpeed = 10.0f;
+    [Header("Opciones de Personaje")]
+
+    public float walkSpeed = 2.5f;
+    public float runSpeed = 7.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     private Vector3 move = Vector3.zero;
 
+    [Header("Opciones de Camara")]
+    
     public Camera cam;
     public float velHorizontal = 3.0f;
     public float velVertical = 2.0f;
     public float limitVerticalMax = 60.0f;
     public float limitVerticalMin = -65.0f;
     float horizontalMouse, verticalMouse;
+
+    [Header("Opciones de Animaciones")]
+
+    public Animator animator;
 
     void Start()
     {
@@ -42,19 +50,28 @@ public class PlayerControler : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 move = transform.TransformDirection(move) * runSpeed;
-            }
-            else
+                animator.SetBool("Runing", true);
+            } else
             {
                 move = transform.TransformDirection(move) * walkSpeed;
+                animator.SetBool("Runing", false);
             }
 
             if (Input.GetKey(KeyCode.Space))
             {
-                move.y = jumpSpeed;   
+                move.y = jumpSpeed;
+                animator.SetBool("Jump", true);
+            }
+            else
+            {
+                animator.SetBool("Jump", false);
             }
         }
         
         move.y -= gravity * Time.deltaTime;
         player.Move(move * Time.deltaTime);
-    }
+
+        animator.SetFloat("velX", Input.GetAxis("Horizontal"));
+        animator.SetFloat("velZ", Input.GetAxis("Vertical"));
+    }   
 }

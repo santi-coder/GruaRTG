a8be2e1e90eecf85eb0dafae785f529e057a1ef3
Santiago Zabib <santiago_zabib@hotmail.com>
2021-08-12 19:31:53 -0300

    Avances cambios de scenas

---
+++ GruaRTG/Assets/Script/CambioScena.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CambioScena : MonoBehaviour
+{
+
+    public Image fundido; 
+
+
+    void Start()
+    {
+        fundido.CrossFadeAlpha(0, 1.0f, false);            
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        StartCoroutine(CambioDeScena());
+    }
+
+    IEnumerator CambioDeScena()
+    {
+        fundido.CrossFadeAlpha(1, 1.0f, false);
+        yield return new WaitForSeconds(1.3f);
+        SceneManager.LoadScene("ColocarIndumentaria");
+    }
+}
